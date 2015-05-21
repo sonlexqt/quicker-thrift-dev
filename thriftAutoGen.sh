9fe8019b08508c8d4d628ef26bf44648c90916c4
thaisonle94 <thaisonle94@gmail.com>
2015-05-21 12:59:10 +0700

    Fix when include to cpp project the 1st time + add more comments

--- thriftAutoGen.sh
+++ thriftAutoGen.sh
@@ -3,8 +3,9 @@
 # Author: Le Thai Son
 # A short shell script to:
 # + Automatically generate native language code from thrift IDL file
-# + Update the newly generated code the the provided project
-# At the moment (Mon, May 18 2015 4:40PM this works best with desiredLanguage=Java || Cpp)
+# + Update the newly generated code to the the provided project
+# At the moment (Mon, May 21 2015 12:52PM this works best with 
+# desiredLanguage=Java || Cpp, IDE = Netbeans)
 
 # Arguments:
 # $1 : name of the desired language
@@ -18,15 +19,21 @@ function editConfigXMLFile()
 	pjDirPath=$2
 	CONTENT="<pElem>gen-$language</pElem>"
 	CONFIGFILE="$pjDirPath/nbproject/configurations.xml"
-	# Check if the element has already existed
+	TEMPFILE="$pjDirPath/nbproject/thrift-autogen-temp.xml"
+	# Check if the include element has already existed
 	if grep -q $CONTENT $CONFIGFILE
 	then
+		# If already existed, no need to insert it to the config file
     	echo "> gen-cpp folder has already been included"
 	else
-    	echo "> gen-cpp folder not has been included yet, attempt to include it to the project"
+		# Else, insert the element to the config file	
+    	echo "> gen-cpp folder has not been included yet, attempting to include it to the project"
     	# Add new element to XML file
     	C=$(echo $CONTENT | sed 's/\//\\\//g')
-		sed "/<\/incDir>/ s/.*/${C}\n&/" CONFIGFILE #TODO cannot add new element to XML file
+    	# Write the new content to a temporary file
+		sed "/<\/incDir>/ s/.*/${C}\n&/" $CONFIGFILE > $TEMPFILE
+		# Replace the current configurations file with the temp file
+		mv -f $TEMPFILE $CONFIGFILE
 	fi
 }
 
@@ -34,16 +41,18 @@ desiredLanguage=$1
 thriftFileFullname=$2
 projectDirPath=$3
 
+# Use thrift to generate native language code
 thrift -r --gen $desiredLanguage $thriftFileFullname
 
 # Get the thriftFileName from the thriftFileFullname
 # Because it thriftFileName is usually the name of the newly created folder
 IFS='.' read -r thriftFileName thriftFileExtension <<< $thriftFileFullname
 
+# Choose the proper way for handling each language
 case $desiredLanguage in
     "java" )
 		echo "> language = java"
-		# Attempt to copy the newly generated java code to the Netbeans project folder
+		# Simply copy & overwrite the newly generated java code to the Netbeans project folder
 		cp -avr gen-$desiredLanguage/$thriftFileName $projectDirPath/src
         ;;
     "cpp" )
@@ -55,6 +64,7 @@ case $desiredLanguage in
 		editConfigXMLFile 'cpp' $projectDirPath
         ;;
     * ) echo "> language = $desiredLanguage"
+		# Other types of language, not implemented yet
 		# Attempt to copy the newly generated code to the project folder
 		cp -avr gen-$desiredLanguage $projectDirPath
 esac
